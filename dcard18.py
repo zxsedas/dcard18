8870024c9ac13b8fb0997bc147a709e99c30dcfe
zxsedas <zxsedas@gmail.com>
2020-08-11 01:18:45 +0800

    'update'

--- dcard18.py
+++ dcard18.py
@@ -11,46 +11,51 @@ class dcard_sex():
     data['id']#id編號
     data['gender']#性別
     """
-    def __init__(self,count,number):
-        self.url="https://www.dcard.tw/service/api/v2/forums/sex/posts?popular=true&limit="
-        self.count=count
-        self.number=number
-        self.j=0
-        self.i=0
-        self.times=-1
+    def __init__(self,count, limit_number):
+        self.url = "https://www.dcard.tw/service/api/v2/forums/sex/posts?popular=true&limit="
+        self.count = count
+        self.limit_number = limit_number
+        self.j = 0
+        self.i = 0
+        self.times = -1
     def titles_article_img(self):
-        data=requests.get(self.url+str(self.number))
-        data_json=json.loads(data.text)
-        while self.count>0:
-            for data in data_json:
-                if self.i>1:
-                    url_article="https://www.dcard.tw/f/sex/p/"+str(data['id'])
-                    print(data['title'],data['id'],data["gender"],url_article,"  ",self.times)
-                    if data['gender'] == "F":
-                        self.crawl_img(url_article)
+        data = requests.get(self.url + str(self.limit_number))
+        data_json_content = json.loads(data.text)
+
+        while self.count > 0:
+            for data in data_json_content:
+                if self.i > 1:
+                    article_id_url = "https://www.dcard.tw/f/sex/p/"+str(data['id'])
+                    print(data['title'],data['id'],data["gender"],article_id_url,"  ", self.times)
+
+                    if data['gender'] == "F":#F=Female
+                        self.crawl_img(article_id_url)
                 self.times+=1
                 self.i+=1
-            url_article="https://www.dcard.tw/service/api/v2/forums/sex/posts?popular=true&limit="+str(self.number)+"&before="+str(data['id'])
-            data=requests.get(url_article)
-            data_json=json.loads(data.text)
+            
+            url_article_before = self.url + str(self.limit_number) + "&before=" + str(data['id'])
+            article_content_before = requests.get(url_article_before).text
+            data_json_content = json.loads(article_content_before)
+
             self.count-=1
 
     def crawl_img(self,url_article):
-        sex_content=requests.get(url_article)
-        soup1=BeautifulSoup(sex_content.text,"html.parser")
-        imgs=soup1.select("article div img")
+        sex_content = requests.get(url_article)
+        sex_soup = BeautifulSoup(sex_content.text, "html.parser")
+        imgs = sex_soup.select("article div img")
         #print(imgs)
+
         for img in imgs:
             #print(img["src"],self.j)
             pic=requests.get(img["src"])
-            sex_img=pic.content
-            pic_out=open("imglib/{}.png".format(self.j),"wb")
+            sex_img = pic.content
+            pic_out = open("imglib/{}.png".format(self.j), "wb")
             pic_out.write(sex_img)
-            self.j+=1
+            self.j += 1
             pic_out.close()
 
 
-tom=dcard_sex(3,5)
+tom=dcard_sex(3,100)
 tom.titles_article_img()
 
 
